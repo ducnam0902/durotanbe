e41899b28f97751c129360b073bd2ddb8b6e4982
nam <ducnam0902@gmail.com>
2023-07-03 23:40:31 +0700

    base | initial project

---
+++ src/test/java/com/durotan/DurotanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.durotan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DurotanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
